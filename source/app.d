28840869f255162e7339b36a9307954d3b9ffb25
theoldmoon0602 <theoldmoon0602@gmail.com>
2018-06-28 22:13:07 +0900

    draw stage

--- source/app.d
+++ source/app.d
@@ -1,6 +1,39 @@
 import std.stdio;
 
+struct P
+{
+  public:
+    long y;
+    long x;
+}
+
+
+void drawStage(const(string[]) stage, const(P) player)
+{
+  foreach (y, l; stage) {
+    foreach (x, c; l) {
+      if (player == P(y, x)) {
+        write('p');
+      }
+      else {
+        write(c);
+      }
+    }
+    writeln();
+  }
+}
+
+
 void main()
 {
-	writeln("Edit source/app.d to start your project.");
+  string[] stage = [
+    "########",
+    "# ..   #",
+    "# oo   #",
+    "#      #",
+    "########",
+  ];
+  auto player = P(1, 5);
+  
+  drawStage(stage, player);
 }
