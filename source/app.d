a639d1f71193cfd3ca8fe502516ab3908cd0f0e3
theoldmoon0602 <theoldmoon0602@gmail.com>
2018-06-29 10:32:38 +0900

    add SDL

--- source/app.d
+++ source/app.d
@@ -1,6 +1,7 @@
 import std.stdio;
 import std.conv;
 import std.algorithm;
+import derelict.sdl2.sdl;
 
 struct P
 {
@@ -11,11 +12,11 @@ struct P
 
 enum Input
 {
+  NONE,
   UP,
   DOWN,
   LEFT,
   RIGHT,
-  EXIT
 }
 
 
@@ -24,6 +25,9 @@ void updateGame(ref char[][] stage, ref P player, Input input)
   P newp = player;
   P dp;
   final switch (input) {
+    case Input.NONE:
+      return;
+      
     case Input.UP:
       newp.y--;
       dp.y--;
@@ -40,22 +44,20 @@ void updateGame(ref char[][] stage, ref P player, Input input)
       newp.x++;
       dp.x++;
       break;
-    case Input.EXIT:
-      throw new Exception("Program error");
   }
-  
+
   if (newp.y < 0 || newp.y >= stage.length) {
     return;
   }
   if (newp.x < 0 || newp.x >= stage[newp.y].length) {
     return;
   }
-  
+
   auto c = stage[newp.y][newp.x];
   if (c == '#') {
     return;
   }
-  
+
   if (c == 'o' || c == 'O') {
     auto c2 = stage[newp.y + dp.y][newp.x + dp.x];
     if (c2 == ' ') {
@@ -69,53 +71,32 @@ void updateGame(ref char[][] stage, ref P player, Input input)
       return;
     }
   }
-  
+
   player = newp;
 }
 
-void drawStage(const(char[][]) stage, const(P) player)
+void drawStage(SDL_Renderer* renderer, const(char[][]) stage, const(P) player)
 {
-  foreach (y, l; stage) {
-    foreach (x, c; l) {
-      if (player == P(y, x)) {
-        if (c == '.') {
-          write('P');
-        }
-        else {
-          write('p');
-        }
-      }
-      else {
-        write(c);
-      }
-    }
-    writeln();
-  }
-}
+  SDL_RenderClear(renderer);
 
-Input getInput()
-{
-  writeln("WASD or Q");
-  while (true) {
-    char c;
-    readf("%c", &c);
-    
-    if (c == 'w') {
-      return Input.UP;
-    }
-    else if (c == 'a') {
-      return Input.LEFT;
-    }
-    else if (c == 's') {
-      return Input.DOWN;
-    }
-    else if (c == 'd') {
-      return Input.RIGHT;
-    }
-    else if (c == 'q') {
-      return Input.EXIT;
-    }
-  }
+  // foreach (y, l; stage) {
+  //   foreach (x, c; l) {
+  //     if (player == P(y, x)) {
+  //       if (c == '.') {
+  //         write('P');
+  //       }
+  //       else {
+  //         write('p');
+  //       }
+  //     }
+  //     else {
+  //       write(c);
+  //     }
+  //   }
+  //   writeln();
+  // }
+
+  SDL_RenderPresent(renderer);
 }
 
 bool checkIsGameClear(const(char[][]) stage)
@@ -127,8 +108,18 @@ bool checkIsGameClear(const(char[][]) stage)
 }
 
 
-void main()
-{
+void main() {
+  DerelictSDL2.load();
+  SDL_Init(SDL_INIT_EVERYTHING);
+
+  auto window = SDL_CreateWindow(
+      "Kurohon",
+      SDL_WINDOWPOS_UNDEFINED,
+      SDL_WINDOWPOS_UNDEFINED,
+      640, 480,
+      SDL_WINDOW_ALLOW_HIGHDPI);
+  auto renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
   auto stage = [
     "########",
     "# ..   #",
@@ -137,16 +128,46 @@ void main()
     "########",
   ].to!(char[][]);
   auto player = P(1, 5);
-  
-  drawStage(stage, player);
+
+  drawStage(renderer, stage, player);
+
   while (true) {
-    auto input = getInput();
-    if (input == Input.EXIT) { break; }
+    SDL_Event e;
+    Input input = Input.NONE;
+    while (SDL_PollEvent(&e) != 0) {
+      if (e.type == SDL_QUIT) {
+        goto quit;
+      }
+      else if (e.type == SDL_KEYDOWN) {
+        switch (e.key.keysym.sym) {
+          case SDLK_w:
+            input = Input.UP;
+            break;
+          case SDLK_a:
+            input = Input.LEFT;
+            break;
+          case SDLK_s:
+            input = Input.DOWN;
+            break;
+          case SDLK_d:
+            input = Input.RIGHT;
+            break;
+          case SDLK_q:
+            goto quit;
+          default:
+            break;
+        }
+      }
+    }
+    
     updateGame(stage, player, input);
-    drawStage(stage, player);
+    drawStage(renderer, stage, player);
     if (checkIsGameClear(stage)) {
-      writeln("Congratulations!");
-      break;
+      goto quit;
     }
   }
+
+quit:
+  SDL_DestroyWindow(window);
+  SDL_Quit();
 }
